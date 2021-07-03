1102a0ce5cc241be04a683bb024b8b4d3f06d979
Upar24 <alfionita.fina24@gmail.com>
2021-07-03 15:40:36 +0700

    fix image user init

--- app/src/main/java/com/example/pwameme/data/local/entities/User.kt
+++ app/src/main/java/com/example/pwameme/data/local/entities/User.kt
@@ -9,7 +9,7 @@ data class User(
     val password: String,
     val following: List<String> = listOf(),
     val followers: List<String> = listOf(),
-    val image: String = "0",
+    val image: String = "R.drawable.image0",
     val bio: String = "",
     val score: Int = 1000,
     @PrimaryKey(autoGenerate = false)
